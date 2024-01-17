0192055f7ca726c8c342f2107c0046d0407ddf3a
Eugene-A-01 <eugene01@yonsei.ac.kr>
2024-01-17 16:09:13 +0900

    Fix: 보일러플레이트 수정-빌드에러 해결

---
+++ src/app/(BeforeLogin)/login/page.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <div>로그인페이지</div>;
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import { ReactNode } from "react";
+
+export default function TempLayout({ children }: { children: ReactNode }) {
+  return <div>{children}</div>;
+}
