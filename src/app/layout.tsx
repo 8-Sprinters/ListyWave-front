7db29ea7e47af7d75ee03b327d1a8d9fa73f59a6
Eugene Ahn <70089733+Eugene-A-01@users.noreply.github.com>
2024-01-22 21:26:58 +0900

    Fix: Hot Reload 에러, Hydration 에러 해결 (#5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,5 +4,14 @@ import React from "react";
 import { ReactNode } from "react";
 
 export default function TempLayout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <html lang="ko">
+      <head>
+        <title>ListyWave</title>
+      </head>
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
 }
