ad1a7205bca9b19faec138e2f3879bf7c3f6ed74
ParkSohyunee <sohyunee016@gmail.com>
2024-01-29 10:30:35 +0900

    Feat: 피드 페이지 - 카테고리 타입에 따른 리스트 목록 불러오기 로직 추가

--- src/app/user/_components/Categories.tsx
+++ src/app/user/_components/Categories.tsx
@@ -1,14 +1,31 @@
-'use client';
+/**
+ TODO
+ - [ ] 카테고리 리스폰스 확인, 리스폰스에 따라 api 요청 로직 수정
+ - [ ] 카테고리 스타일링
+ - [ ] 클릭했을때 로직 (상위요소에 핸들러 고민)
+ */
 
 import { KINDS } from '../mockData/categories'; // 삭제 예정
 
-export default function Categories() {
+interface CategoriesProps {
+  // onClick: (e: MouseEvent<HTMLDivElement>) => void;
+  onClick: (kind: string) => void;
+}
+
+export default function Categories({ onClick }: CategoriesProps) {
   // 카테고리 api 요청
+
+  const handleChangeCategory = (kind: string) => () => {
+    // console.log(kind);
+    onClick(kind);
+  };
 
   return (
     <div>
       {KINDS.map((kind) => (
-        <button key={kind}>{kind}</button>
+        <button key={kind} onClick={handleChangeCategory(kind)}>
+          {kind}
+        </button>
       ))}
     </div>
   );
--- src/app/user/_components/Content.tsx
+++ src/app/user/_components/Content.tsx
@@ -3,11 +3,16 @@
 /**
  TODO
  - [x] 콜라보 리스트 눌렀을 때 
- - [ ] 카테고리 눌렀을 때
- - [ ] 리스트 타입을 고정할 수 있는 방법
+ - [x] 카테고리 눌렀을 때
+ - [x] 리스트 타입을 고정할 수 있는 방법 - stateHook or searchParamsHook
+ - [ ] api 연동
+ - [ ] 스타일링
+ - [ ] 무한스크롤 적용
+ - [ ] 피드페이지 스켈레톤 ui 적용
  */
 
-import { MouseEvent } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { MouseEvent, useState } from 'react';
 
 import { ListType, UserType } from '../mockData/mockDataTypes';
 import { LISTS_ME } from '../mockData/lists';
@@ -20,6 +25,11 @@ interface ContentProps {
 }
 
 export default function Content({ user }: ContentProps) {
+  const [listType, setListType] = useState('my'); // or useReducer? 왜냐하면, 렌더링 필요없으므로
+  const searchParams = useSearchParams();
+
+  console.log(searchParams.get('type')); // 삭제 예정
+
   // userId로 피드 정보 가져오는 api 요청
   // /users/{userId}/feeds?type={}&category={}
   // default는 type=my, category=all
@@ -31,8 +41,16 @@ export default function Content({ user }: ContentProps) {
 
     const type = (e.target as HTMLButtonElement).id;
     console.log(type); // 삭제 예정
+    setListType(type);
+    // userId, type, category(전체)로 피드 정보 가져오는 api 요청
+  };
 
-    // userId, type으로 피드 정보 가져오는 api 요청
+  const handleFetchListsOnCategory = (kind: string) => {
+    console.log(kind); // 삭제 예정
+    // 현재 위치한 타입을 어떻게 파악? -> state 필요?
+    console.log(listType); // 삭제 예정
+
+    // userId, type, category로 피드 정보 가져오는 api 요청
   };
 
   return (
@@ -42,7 +60,7 @@ export default function Content({ user }: ContentProps) {
         <button id="my">마이 리스트</button>
         <button id="collabo">콜라보 리스트</button>
       </div>
-      <Categories />
+      <Categories onClick={handleFetchListsOnCategory} />
       <div>
         {LISTS_ME.map((list: ListType) => (
           <ul key={list.listId}>
