527d122fcdb38e0e6561ae18b124f3c1a7dee55d
Nahyun <tarea202001@gmail.com>
2024-01-31 01:40:30 +0900

    feat: 헤더 로직 구현

--- src/components/ListDetailOuter/Header/Header.tsx
+++ src/components/ListDetailOuter/Header/Header.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState, MouseEvent } from 'react';
+import { useRouter, useParams } from 'next/navigation';
 
 import * as styles from './Header.css';
 import ModalPortal from '@/components/ModalPortal';
@@ -11,6 +12,8 @@ import KebabButton from '/public/icons/vertical_kebab_button.svg';
 
 function Header() {
   const [isSheetActive, setSheetActive] = useState<boolean>(false);
+  const router = useRouter();
+  const params = useParams<{ userNickname: string; listId: string }>();
 
   const handleSheetActive = () => {
     setSheetActive((prev: boolean) => !prev);
@@ -22,6 +25,14 @@ function Header() {
     }
   };
 
+  const handleBackButtonClick = () => {
+    router.back();
+  };
+
+  const handleHistoryButtonClick = () => {
+    router.push(`/${params.userNickname}/${params.listId}/history`);
+  };
+
   return (
     <>
       {isSheetActive && (
@@ -30,12 +41,12 @@ function Header() {
         </ModalPortal>
       )}
       <div className={styles.wrapper}>
-        <button className={styles.buttonResetStyle}>
+        <button className={styles.buttonResetStyle} onClick={handleBackButtonClick}>
           <BackButton className={styles.svgCommonStyles} />
         </button>
         <div className={styles.title}>리스트</div>
         <div className={styles.headerRightWrapper}>
-          <button className={styles.buttonResetStyle}>
+          <button className={styles.buttonResetStyle} onClick={handleHistoryButtonClick}>
             <HistoryButton className={styles.svgCommonStyles} />
           </button>
           <button className={styles.buttonResetStyle} onClick={handleSheetActive}>
