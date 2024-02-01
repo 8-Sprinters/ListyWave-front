57169411d550c2f0ec95f42728d301b427de52e9
Nahyun <tarea202001@gmail.com>
2024-02-01 21:46:41 +0900

    Chore: commitlint 설정 수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,8 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-empty': [2, 'never'], //타입은 비어있을 수 없다.
-    'type-enum': [2, 'always', ['Chore', 'Feat', 'Fix', 'Refactor', 'Enhance', 'Design', 'Style']],
-    'type-case': [2, 'always', 'pascal-case'],
-    // 'type-enum': [2, 'always', /^(Chore|Feat|Fix|Refactor|Enhance|Design|Style)$/i],
+    'type-enum': [2, 'always', ['Chore', 'Feat', 'Fix', 'Refactor', 'Enhance', 'Design', 'Style']], //타입은 다음만 작성할 수 있다.
+    'type-case': [2, 'always', 'pascal-case'], //타입의 첫글자는 대문자로 시작해야한다.
   },
 };
