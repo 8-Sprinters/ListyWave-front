73d922767e02935cc1625b2b9c681b15ae3981ad
Seoyoung <seoyoungiyo@gmail.com>
2024-02-03 03:01:07 +0900

    Feat: 링크 미리보기 컴포넌트 구현

---
+++ src/app/create/_components/LinkPreview.css.ts
@@ -0,0 +1,25 @@
+import { style } from '@vanilla-extract/css';
+
+export const previewBox = style({
+  width: '90px',
+  height: '90px',
+
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  position: 'relative',
+
+  backgroundColor: '#EBF4FF',
+
+  borderRadius: '10px',
+  whiteSpace: 'pre-wrap',
+  overflow: 'hidden',
+});
+
+export const clearButton = style({
+  position: 'absolute',
+  top: '5px',
+  right: '5px',
+});
---
+++ src/app/create/_components/LinkPreview.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+import ClearBlackIcon from '/public/icons/clear_x_black.svg';
+import LinkIcon from '/public/icons/link.svg';
+import * as styles from './LinkPreview.css';
+
+interface LinkPreviewProps {
+  type: 'link' | 'image';
+  url?: string;
+  domain?: string;
+  imageUrl?: string;
+  handleClearButtonClick: () => void;
+}
+
+export default function PreviewBox({ type, url, domain, imageUrl, handleClearButtonClick }: LinkPreviewProps) {
+  const handlePreviewClick = () => {
+    window.open(url);
+  };
+
+  return (
+    <div className={styles.previewBox} onClick={handlePreviewClick} role="button">
+      {type === 'link' && (
+        <>
+          <LinkIcon fill="#61646B" />
+          <p>{domain}</p>
+        </>
+      )}
+      {type === 'image' && <Image src={imageUrl ?? ''} alt="첨부 이미지" width="90" height="90" />}
+      <button className={styles.clearButton} onClick={handleClearButtonClick}>
+        <ClearBlackIcon alt="링크 삭제 버튼" />
+      </button>
+    </div>
+  );
+}
