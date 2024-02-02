a3eaf2af377d3bb33fe129ae3ff6b1ce347fdbf4
seoyoung-min <seoyoungiyo@gmail.com>
2024-02-03 05:08:58 +0900

    Refactor: ModalButton 비활성화 속성 추가

--- src/components/Modal/ModalButton.tsx
+++ src/components/Modal/ModalButton.tsx
@@ -5,15 +5,16 @@ interface ModalButtonProps {
   children: ReactNode;
   onCancel: MouseEventHandler<HTMLButtonElement>;
   onClick: MouseEventHandler<HTMLButtonElement>;
+  isDisabled?: boolean;
 }
 
-export default function ModalButton({ children, onCancel, onClick }: ModalButtonProps) {
+export default function ModalButton({ children, onCancel, onClick, isDisabled = false }: ModalButtonProps) {
   return (
     <div className={styles.buttonContainer}>
       <button type="button" className={styles.secondaryButton} onClick={onCancel}>
         취소
       </button>
-      <button type="button" className={styles.primaryButton} onClick={onClick}>
+      <button type="button" className={isDisabled ? styles.disabledButton : styles.primaryButton} onClick={onClick} disabled={isDisabled}>
         {children}
       </button>
     </div>
