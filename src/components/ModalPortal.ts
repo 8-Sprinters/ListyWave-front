f63caeef8f28a42a9996ab1b34633bb00dc0001a
seoyoung-min <seoyoungiyo@gmail.com>
2024-02-03 03:31:10 +0900

    Refactor: CreatePortal 임포트 방식 변경 및 포탈 성능 개선

--- src/components/ModalPortal.ts
+++ src/components/ModalPortal.ts
@@ -1,14 +1,18 @@
 import { ReactNode } from 'react';
-import ReactDOM from 'react-dom';
+import { createPortal } from 'react-dom';
 
 interface Props {
   children: ReactNode;
 }
 
 const ModalPortal = ({ children }: Props) => {
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
   const el = document.getElementById('modal-root') as HTMLElement;
 
-  return ReactDOM.createPortal(children, el);
+  return createPortal(children, el);
 };
 
 export default ModalPortal;
