d7c9f02cad5b6906db566a1f0aae0b06368d55bd
Seoyoung <seoyoungiyo@gmail.com>
2024-02-03 02:49:51 +0900

    Design: 글로벌css 설정

---
+++ src/styles/globalStyles.css.ts
@@ -0,0 +1,39 @@
+import { globalStyle } from '@vanilla-extract/css';
+
+globalStyle('html', {
+  fontSize: '62.5%',
+  color: '#19191B', //글자기본색
+});
+
+globalStyle('*', {
+  boxSizing: 'border-box',
+  fontFamily: 'Pretendard',
+});
+
+globalStyle('html, body, div, span, h1, h2, h3, h4, h5, h6, p, a, dl, dt, dd, ol, ul, li, form, label, table, button', {
+  margin: 0,
+  padding: 0,
+  border: 0,
+});
+
+globalStyle('a', {
+  textDecoration: 'none !important',
+});
+
+globalStyle('ol, ul', {
+  listStyle: 'none',
+});
+
+globalStyle('button', {
+  cursor: 'pointer',
+  backgroundColor: 'transparent',
+});
+
+globalStyle('input', {
+  border: 'none',
+  padding: 'none',
+});
+
+globalStyle('input:focus', {
+  outline: 'none',
+});
