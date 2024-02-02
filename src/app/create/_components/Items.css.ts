0c32f9435114f4437500387557cb4afc0ffef64f
Seoyoung <seoyoungiyo@gmail.com>
2024-02-03 03:15:41 +0900

    Feat: 아이템 리스트 컴포넌트 및 상수 파일 추가

---
+++ src/app/create/_components/Items.css.ts
@@ -0,0 +1,86 @@
+import { style } from '@vanilla-extract/css';
+
+export const itemsContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '16px',
+});
+
+export const item = style({
+  padding: '12px 18px',
+
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '12px',
+
+  backgroundColor: '#fff',
+
+  fontSize: '1.6rem',
+  border: 'solid 1px #AFB1B6',
+  borderRadius: '6px',
+
+  transition: 'box-shadow 0.3s ease',
+  boxShadow: 'rgba(0, 0, 0, 0.1) 0px 2px 2px;',
+});
+
+export const draggingItem = style([
+  item,
+  {
+    boxShadow: '0px 20px 50px -5px #AFB1B6',
+  },
+]);
+
+export const title = style({
+  //body1
+  fontSize: '1.6rem',
+  fontWeight: '400',
+  lineHeight: '1.6rem',
+  letterSpacing: '-0.48px',
+
+  '::placeholder': {
+    color: '#AFB1B6',
+  },
+});
+
+export const comment = style({
+  width: '100%',
+  resize: 'none',
+
+  flexGrow: '1',
+
+  //body2
+  fontSize: '1.5rem',
+  lineHeight: '2.5rem',
+  letterSpacing: '-0.45px',
+
+  border: 'none',
+  outline: 'none',
+
+  '::placeholder': {
+    color: '#AFB1B6',
+  },
+});
+
+export const modalInput = style([
+  title,
+  {
+    width: '100%',
+    padding: '8px',
+
+    border: 'solid 1px #AFB1B6',
+    borderRadius: '4px',
+  },
+]);
+
+export const countLength = style({
+  //body2
+  fontSize: '1.5rem',
+  letterSpacing: '-0.45px',
+  color: '#61646B',
+});
+
+export const error = style({
+  flexShrink: '0',
+  color: '#FF5454',
+  fontSize: '1.5rem',
+});
