f325a2fde97a347757b1876cd68b37d27f94517b
seoyoung-min <seoyoungiyo@gmail.com>
2024-02-03 05:30:43 +0900

    Fix: 프리티어 줄바꿈 에러 수정

--- src/components/Modal/ModalButton.tsx
+++ src/components/Modal/ModalButton.tsx
@@ -14,7 +14,12 @@ export default function ModalButton({ children, onCancel, onClick, isDisabled = 
       <button type="button" className={styles.secondaryButton} onClick={onCancel}>
         취소
       </button>
-      <button type="button" className={isDisabled ? styles.disabledButton : styles.primaryButton} onClick={onClick} disabled={isDisabled}>
+      <button
+        type="button"
+        disabled={isDisabled}
+        className={isDisabled ? styles.disabledButton : styles.primaryButton}
+        onClick={onClick}
+      >
         {children}
       </button>
     </div>
