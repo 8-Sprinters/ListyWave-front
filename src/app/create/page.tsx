67943b7f091b498622a6d288657345a23bd98b9e
Eugene Ahn <70089733+Eugene-A-01@users.noreply.github.com>
2024-02-03 11:07:19 +0900

    Feat: 리스트 생성 페이지 구현 (#12)

---
+++ src/app/create/page.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+//page.tsx
+import { FieldErrors, FormProvider, useForm } from 'react-hook-form';
+// import CreateItem from '@/app/create/_components/CreateItem';
+import CreateList from '@/app/create/_components/CreateList';
+
+interface Item {
+  rank: number;
+  title: string;
+  comment: string | null;
+  link: string | null;
+}
+
+interface FormValues {
+  ownerId: number;
+  category: string;
+  labels: string[];
+  collaboratorIds: number[];
+  title: string;
+  description: string;
+  isPublic: boolean;
+  backgroundColor: string;
+  items: Item[];
+}
+
+export type FormErrors = FieldErrors<FormValues>;
+
+export default function CreatePage() {
+  const methods = useForm<FormValues>({
+    mode: 'onChange',
+    defaultValues: {
+      ownerId: 0,
+      category: '문화',
+      labels: [],
+      collaboratorIds: [],
+      title: '',
+      description: '',
+      isPublic: true,
+      backgroundColor: '#FFFFFF',
+      items: [
+        {
+          rank: 0,
+          title: '',
+          comment: null,
+          link: null,
+        },
+        {
+          rank: 0,
+          title: '',
+          comment: null,
+          link: null,
+        },
+        {
+          rank: 0,
+          title: '',
+          comment: null,
+          link: null,
+        },
+      ],
+    },
+  });
+  const itemValue = methods.watch('items');
+
+  //rank 정리하는 함수. 마지막에 한 번만 실행되게끔 하기 (submit)
+  itemValue.forEach((item, index) => {
+    item.rank = index + 1;
+  });
+
+  return (
+    <div>
+      <div>
+        <FormProvider {...methods}>
+          {/* <CreateItem /> */}
+          <CreateList />
+        </FormProvider>
+      </div>
+    </div>
+  );
+}
