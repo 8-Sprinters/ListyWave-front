a55bda12cf89eef7a50101af478e6006fcb60fbf
ParkSohyunee <sohyunee016@gmail.com>
2024-02-03 17:22:07 +0900

    Chore: axios 설정

--- src/lib/axios/axiosInstance.ts
+++ src/lib/axios/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_DOMAIN,
+  baseURL: 'https://dev.api.listywave.com',
+  withCredentials: true,
 });
 
 export default axiosInstance;
