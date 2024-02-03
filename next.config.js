22156948f8661782bca146be35c23a5db108e91f
ParkSohyunee <sohyunee016@gmail.com>
2024-02-03 22:34:28 +0900

    Feat: 카카오 리다이렉트 페이지에서 서버로 최종 로그인 요청 기능 구현

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+};
 
 module.exports = withVanillaExtract(nextConfig);
