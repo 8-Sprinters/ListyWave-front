008ae6586fc5b02c1b4dd067d33664d65253103e
ParkSohyunee <sohyunee016@gmail.com>
2024-02-04 12:07:18 +0900

    Refactor: 프로미스를 async/await로 구현, 에러 메세지 처리

--- src/app/auth/redirect/kakao/page.tsx
+++ src/app/auth/redirect/kakao/page.tsx
@@ -2,8 +2,8 @@
 
 import { useSearchParams, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import { AxiosError } from 'axios';
 
-import authKakao from '@/app/_api/auth';
 import axiosInstance from '@/lib/axios/axiosInstance';
 
 export default function KakaoRedirectPage() {
@@ -11,48 +11,34 @@ export default function KakaoRedirectPage() {
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
 
-  // const loginKakao = async (code: string) => {
-  //   try {
-  //     console.log('api요청');
-  //     const res = await axiosInstance.get(`/auth/redirect/kakao?code=${code}`, {
-  //       signal: controller.signal,
-  //     });
-  //     console.log(res); // 삭재 얘정
-  //     if (res.data) {
-  //       console.log('api 요청 완료');
-  //       router.push('/');
-  //     }
-  //   } catch (error) {
-  //     console.error(error);
-  //     console.log(error);
-  //   }
-  // };
-
   useEffect(() => {
     const controller = new AbortController();
 
     console.log('마운트');
 
-    if (code) {
-      console.log('api 요청');
+    if (!code) return;
 
-      axiosInstance
-        .get(`/auth/redirect/kakao?code=${code}`, {
+    const loginKakao = async () => {
+      try {
+        const res = await axiosInstance.get(`/auth/redirect/kakao?code=${code}`, {
           signal: controller.signal,
-        })
-        .then((res) => {
-          console.log('axios 요청 완료');
-          console.log(res.data);
+        });
+        console.log('axios 요청 완료');
+        console.log(res.data);
 
-          router.push('/');
-        })
-        .catch((error) => console.error(error.message));
-    }
+        router.push('/');
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          if (!controller.signal.aborted) {
+            console.error(error.message);
+          } else {
+            console.log('Request canceled:', error.message);
+          }
+        }
+      }
+    };
 
-    // if (code) {
-    //   console.log(code); // 삭제 예쩡
-    //   loginKakao(code);
-    // }
+    loginKakao();
 
     return () => {
       console.log('마운트 해제 및 axios 요청 취소');
