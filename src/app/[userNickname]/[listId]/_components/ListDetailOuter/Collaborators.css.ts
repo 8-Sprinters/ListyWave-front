450c256aa8c891b443d546444a27a387a5db18b3
강나현 <142777396+Nahyun-Kang@users.noreply.github.com>
2024-02-04 16:10:24 +0900

    Feat: 리스트 상세페이지 바깥쪽 레이아웃 퍼블리싱 (#14)
    * feat: timediff 유틸함수 파일 생성
    
    * feat: timeDiff 유틸 함수 구현
    
    * feat: 댓글 레이아웃 구현 완료
    
    * feat: 프로필 이미지 콜렉션 레이아웃 구현
    
    * feat: 바텀시트 내용 수정
    
    * feat: 바텀시드 정리
    
    * feat: Header, BottomSheet 타입 정리
    
    * design: css 적용 완료
    
    * feat: collaboratorsPopOver 구현
    
    * feat: 헤더 로직 구현
    
    * design: css 수정
    
    * design: css 수정 (collaborators, defaultProfile 등)
    
    * design: css 수정(collaboratorsPopOver, comment, defaultProfile)
    
    * feat: label 공용 컴포넌트화
    
    * design: label margin right 수정
    
    * design: 바텀시트 css 수정
    
    * Refactor: 라벨 컴포넌트 타입 및 스타일 수정
    
    * Chore: 폴더 구조 정리
    
    * Style : timeDiff 함수 jsdoc 작성
    
    * Feat: 삭제 모달 구현
    
    * Refactor: Comments.css.ts 분리
    
    * Chore: utils init.ts 삭제
    
    * Chore: 충돌 해결
    
    * Chore: 충돌 해결
    
    * Refactor: replies.tsx 타입 분리 및 img alt 태그 추가
    
    * Refactor: 타입 분리
    
    * Refactor: 바텀시드 공통화
    
    * Style: img 태그 alt 추가
    
    * Refactor: 모달 컴포넌트 수정 내용 반영
    
    * Style: 사용하지 않는 import문 정리
    
    * Design: 댓글 input css 수정
    
    * Feat: delete modal 공용 컴포넌트 수정 내용 반영
    
    * Fix: next.config.js 파일 에러 수정
    
    * Refactor: comment.tsx 필요없는 코드 삭제
    
    * Refactor: img 태그 Image 로 교체, import문 공백 삭제
    
    * Refactor: replies.tsx에도 delete modal 반영
    
    * Refactor: deletemodal 컴포넌트 deletemodalbutton으로 이름 변경
    
    * Design: 바텀시트 backgroud css 수정
    
    * Refactor: deletemodalbutton import문도 컴포넌트와 동일하게 설정
    
    * Style: import문 순서 수정 및 일부 공백 조정
    
    * Refactor: map key 고유 아이디로 지정
    
    * Fix: 콘솔 에러나는 부분 수정
    
    * Chore: 폰트 파일들 삭제
    
    * Style: 필요없는 주석 삭제
    
    * Feat: .env .gitignore 처리
    
    * Chore: tsconfig.json에 잘못 들어간 코드 삭제
    
    * Chore: settings.json 위치 변경
    
    * Refactor: 코드 리뷰 반영 수정
    
    * Fix: 캐시 삭제 후 gitignore 재설정
    
    * Refactor: modal portal 코드리뷰 반영 수정
    
    * Refactor: 바텀시트 공용 훅 사용하도록 수정
    
    * Design: 버튼 css 수정
    
    * Fix: 버튼 안의 버튼 컴포넌트로 인해 발생하는 오류 수정
    
    * Design: 라벨 아이템에 flexShrink 속성 추가
    
    * Fix: svg icon 충돌 오류 해결
    
    * Fix: 플러스 아이콘 및 collaborators 컴포넌트 오류 해결

---
+++ src/app/[userNickname]/[listId]/_components/ListDetailOuter/Collaborators.css.ts
@@ -0,0 +1,61 @@
+import { style } from '@vanilla-extract/css';
+
+export const collaboratorWrapper = style({
+  position: 'relative',
+  display: 'flex',
+});
+
+export const wrapper = style({
+  display: 'flex',
+  flexDirection: 'row-reverse',
+  justifyContent: 'center',
+  alignItems: 'cemter',
+  transform: 'translateZ(0px)',
+});
+
+export const ProfileImg = style({
+  marginRight: '-10px',
+  width: '36px',
+  height: '36px',
+
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  outline: '3px solid #ffffff',
+  borderRadius: '9999px',
+});
+
+export const profilePlus = style({
+  backgroundColor: '#AEB0B6',
+});
+
+export const profileText = style({
+  color: '#DEE7EE',
+  fontSize: '2rem',
+});
+
+export const collaboratorTitle = style({
+  marginRight: '11px',
+
+  fontSize: '1rem',
+  fontWeight: 600,
+});
+
+export const collaboratorsPopOverWrapper = style({
+  display: 'none',
+
+  selectors: {
+    [`${collaboratorWrapper}:hover &`]: {
+      display: 'block',
+    },
+  },
+});
+
+export const defaultProfile = style({
+  width: '36px',
+  height: '36px',
+
+  outline: '3px solid #ffffff',
+  borderRadius: '9999px',
+});
---
+++ src/app/[userNickname]/[listId]/_components/ListDetailOuter/DeleteModalButton.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react';
+import Modal from '@/components/Modal/Modal';
+import useBooleanOutput from '@/hooks/useBooleanOutput';
+import * as styles from './ModalButtonStyle.css';
+import DeleteButton from '/public/icons/trash_can.svg';
+
+interface DeleteModalProps {
+  children?: ReactNode;
+}
+
+export default function DeleteModal({ children }: DeleteModalProps) {
+  const { isOn, handleSetOff, handleSetOn } = useBooleanOutput(); //모달 열림,닫힘 상태 관리
+  const handleConfirmButtonClick = () => {
+    //확인버튼 클릭시 실행될 로직()
+    handleSetOff(); //닫기
+  };
+
+  return (
+    <>
+      {/*👆 누르면 모달이 열리는 트리거 버튼*/}
+      <button onClick={handleSetOn} className={styles.resetButtonStyle}>
+        <DeleteButton alt="삭제 버튼" />
+      </button>
+
+      {/*✨ 조합한 모달 */}
+      {isOn && (
+        <Modal handleModalClose={handleSetOff}>
+          <Modal.Title>정말로 삭제하시겠습니까?</Modal.Title>
+          {children}
+          <Modal.Button onCancel={handleSetOff} onClick={handleConfirmButtonClick}>
+            확인
+          </Modal.Button>
+        </Modal>
+      )}
+    </>
+  );
+}
