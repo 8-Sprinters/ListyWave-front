0462dd395665aac0473eb4e12b785586fdebfc3a
kanglocal <develocal25@gmail.com>
2024-02-05 14:15:23 +0900

    Style : CI/CD에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,22 +20,23 @@ export default function TempLayout({ children }: { children: ReactNode }) {
 
   return (
     <html lang="ko">
-    <head>
-      <title>ListyWave</title>
-      <Script
-        src="https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js"
-        integrity="sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8"
-        crossOrigin="anonymous"
-        onLoad={kakaoInit}
-      ></Script>
-    </head>
-    <body>
-    <QueryClientProvider client={queryClient}>
-      <div id="modal-root" />
-      <div>{children}</div>
-      <ToastContainer />
-    </QueryClientProvider>
-    </body>
+      <head>
+        <title>ListyWave</title>
+
+        <Script
+          src="https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js"
+          integrity="sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8"
+          crossOrigin="anonymous"
+          onLoad={kakaoInit}
+        ></Script>
+      </head>
+      <body>
+        <QueryClientProvider client={queryClient}>
+          <div id="modal-root" />
+          <div>{children}</div>
+          <ToastContainer />
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
