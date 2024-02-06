61cdb6b2e42f48e9ae7d2bc8fb6ddd3e93b535fc
ParkSohyunee <sohyunee016@gmail.com>
2024-02-06 13:37:50 +0900

    Feat: 리스트 생성, 리스트 조회 api headers 옵션 추가

--- src/app/_api/list/createList.ts
+++ src/app/_api/list/createList.ts
@@ -1,8 +1,14 @@
 import axiosInstance from '@/lib/axios/axiosInstance';
 import { ListCreateType, ListIdType } from '@/lib/types/listType';
 
+const accessToken = '';
+
 export const createList = async (data: ListCreateType) => {
-  const response = await axiosInstance.post<ListIdType>('/lists', data);
+  const response = await axiosInstance.post<ListIdType>('/lists', data, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
 
   return response.data;
 };
--- src/app/_api/list/getAllList.ts
+++ src/app/_api/list/getAllList.ts
@@ -1,9 +1,14 @@
 import axiosInstance from '@/lib/axios/axiosInstance';
 import { AllListType } from '@/lib/types/listType';
 
-export async function getAllList(userId: number) {
-  // const query =
-  const response = await axiosInstance.get<AllListType>(`/users/${userId}/list`);
+const accessToken = '';
+
+export async function getAllList(userId = 4) {
+  const response = await axiosInstance.get<AllListType>(`/users/${userId}/lists?type=my&category=entire&size=5`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
 
   return response.data;
 }
