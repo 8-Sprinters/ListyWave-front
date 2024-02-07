bc02ea154bc24d95d8a4ac1d02032f4b5ea189e4
Seoyoung <seoyoungiyo@gmail.com>
2024-02-07 19:46:00 +0900

    Style: pretendard 로컬 폰트 적용

---
+++ src/styles/pretendardFont.css.ts
@@ -0,0 +1,48 @@
+import { globalFontFace } from '@vanilla-extract/css';
+
+export const Pretendard = 'GlobalPretendard';
+
+globalFontFace(Pretendard, {
+  fontWeight: 900,
+  src: `url('/fonts/Pretendard-Black.woff2') format('woff2')`,
+});
+
+globalFontFace(Pretendard, {
+  fontWeight: 800,
+  src: `url('/fonts/Pretendard-ExtraBold.woff2') format('woff2')`,
+});
+
+globalFontFace(Pretendard, {
+  fontWeight: 700,
+  src: `url('/fonts/Pretendard-Bold.woff2') format('woff2')`,
+});
+
+globalFontFace(Pretendard, {
+  fontWeight: 600,
+  src: `url('/fonts/Pretendard-SemiBold.woff2') format('woff2')`,
+});
+
+globalFontFace(Pretendard, {
+  fontWeight: 500,
+  src: `url('/fonts/Pretendard-Medium.woff2') format('woff2')`,
+});
+
+globalFontFace(Pretendard, {
+  fontWeight: 400,
+  src: `url('/fonts/Pretendard-Regular.woff2') format('woff2')`,
+});
+
+globalFontFace(Pretendard, {
+  fontWeight: 300,
+  src: `url('/fonts/Pretendard-Light.woff2') format('woff2')`,
+});
+
+globalFontFace(Pretendard, {
+  fontWeight: 200,
+  src: `url('/fonts/Pretendard-ExtraLight.woff2') format('woff2')`,
+});
+
+globalFontFace(Pretendard, {
+  fontWeight: 100,
+  src: `url('/fonts/Pretendard-Thin.woff2') format('woff2')`,
+});
