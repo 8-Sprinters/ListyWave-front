a700f4e18d466d9529362988b9c9abc6099e0cf1
Nahyun-Kang <tarea202001@gmail.com>
2024-02-08 11:57:01 +0900

    Fix: 폴더구조 바꾸면서 생긴 경로 에러 처리

--- src/app/user/[userId]/list/[listId]/page.tsx
+++ src/app/user/[userId]/list/[listId]/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import * as styles from './ListDetail.css';
 import Comments from '@/app/user/[userId]/list/[listId]/_components/ListDetailOuter/Comments';
 import Header from '@/app/user/[userId]/list/[listId]/_components/ListDetailOuter/Header';
