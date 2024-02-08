371c219fb9e7a1982b81e24131d7d12273a1f363
ParkSohyunee <sohyunee016@gmail.com>
2024-02-08 20:55:40 +0900

    Style: 불필요한 코드 정리

--- src/app/_api/auth.ts
+++
@@ -1,7 +0,0 @@
-import axiosInstance from '@/lib/axios/axiosInstance';
-
-export default async function authKakao(code: string) {
-  const response = await axiosInstance.get(`/auth/redirect/kakao?code=${code}`);
-
-  return response.data;
-}
