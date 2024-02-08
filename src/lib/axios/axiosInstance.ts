698f23834769eaf96ea31cb2e4f912cdd82b6995
ParkSohyunee <sohyunee016@gmail.com>
2024-02-08 19:30:20 +0900

    Feat: request 요청시, authorization headers에 accessToken 함께 보내는 axios 인터셉터 설정 추가

--- src/lib/axios/axiosInstance.ts
+++ src/lib/axios/axiosInstance.ts
@@ -1,8 +1,25 @@
 import axios from 'axios';
+import { useUser } from '@/store/useUser';
 
 const axiosInstance = axios.create({
   baseURL: 'https://dev.api.listywave.com',
-  withCredentials: true,
+  withCredentials: true, // refreshToken을 고려해서 true로 설정
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = useUser.getState().user.accessToken;
+
+    if (accessToken) {
+      config.headers.Authorization = `${accessToken}`; // Bearer option 추가 예정
+    }
+
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
