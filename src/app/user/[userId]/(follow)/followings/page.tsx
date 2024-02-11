247c9b6e4dab7ef224cf773f7d79dc10b7a2ff55
Eugene-A-01 <eugene01@yonsei.ac.kr>
2024-02-11 22:12:51 +0900

    Feat: 팔로잉, 팔로워 조회 페이지 웹 퍼블리싱 완료

--- src/app/user/[userId]/(follow)/followings/page.tsx
+++ src/app/user/[userId]/(follow)/followings/page.tsx
@@ -1,3 +1,12 @@
-export default function FollowersPage() {
-  return <div>팔로잉 페이지</div>;
+import Header from '../_components/Header';
+import UserList from '../_components/UserList';
+import { mockFollowers } from '../_components/mockData';
+
+export default function FollowingPage() {
+  return (
+    <div>
+      <Header title="팔로잉" />
+      <UserList type="following" list={mockFollowers} />
+    </div>
+  );
 }
