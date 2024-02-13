7db4ace425f189af6801a88b741194b1a1a11c99
Eugene-A-01 <eugene01@yonsei.ac.kr>
2024-02-13 19:18:19 +0900

    Feat: 리스트 수정 페이지 - 아이템 타이틀 수정불가 정책 구현

--- src/app/list/create/_components/CreateItem.tsx
+++ src/app/list/create/_components/CreateItem.tsx
@@ -8,9 +8,10 @@ interface CreateItemProps {
   onBackClick: () => void;
   onSubmitClick: () => void;
   isSubmitting: boolean;
+  type: 'create' | 'edit';
 }
 
-export default function CreateItem({ onBackClick, onSubmitClick, isSubmitting }: CreateItemProps) {
+export default function CreateItem({ onBackClick, onSubmitClick, isSubmitting, type }: CreateItemProps) {
   const {
     formState: { isValid },
   } = useFormContext();
@@ -27,7 +28,7 @@ export default function CreateItem({ onBackClick, onSubmitClick, isSubmitting }:
           최소 3개, 최대 10개까지 아이템을 추가할 수 있어요. <br />
           아이템의 순서대로 순위가 정해져요.
         </p>
-        <Items />
+        <Items disabled={type === 'edit'} />
       </div>
     </div>
   );
