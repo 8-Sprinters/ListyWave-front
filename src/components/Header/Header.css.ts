9ac1b079c62630c6d5aa990a7ae8797b5610d3a5
Seoyoung <seoyoungiyo@gmail.com>
2024-02-15 04:46:08 +0900

    Design: 헤더 높이 변경에 따른 CSS 수정

--- src/components/Header/Header.css.ts
+++ src/components/Header/Header.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const header = style({
   width: '100%',
-  height: '90px',
+  height: '70px',
   paddingLeft: '20px',
   paddingRight: '20px',
 
