0c7d9f8efa65507c12c3732fdc174437e2e19c1e
Seoyoung <seoyoungiyo@gmail.com>
2024-02-14 16:58:57 +0900

    Feat: 프로필 수정 및 이미지 업로드 api 추가

---
+++ src/app/_api/user/updateProfile.ts
@@ -0,0 +1,60 @@
+import axiosInstance from '@/lib/axios/axiosInstance';
+import axios from 'axios';
+import { UserProfileInfoType, UserProfileEditType } from '@/lib/types/userProfileType';
+import compressFile from '@/lib/utils/compressFile';
+
+//프로필수정 이미지업로드 타입
+interface UserPresignedUrlsType {
+  ownerId: number;
+  backgroundPresignedUrl: string;
+  profilePresignedUrl: string;
+}
+
+interface updateProfileParams {
+  userId: Number;
+  data: UserProfileEditType;
+}
+
+const updateProfile = async ({ userId, data }: updateProfileParams) => {
+  const { nickname, description, backgroundImageUrl, profileImageUrl, newBackgroundFileList, newProfileFileList } =
+    data;
+
+  //프로필 수정
+  const profileData: UserProfileInfoType = {
+    nickname,
+    description,
+    backgroundImageUrl,
+    profileImageUrl,
+  };
+  const result = await axiosInstance.patch(`/users/${userId}`, profileData);
+
+  if (result.status === 204 && (newBackgroundFileList !== null || newProfileFileList !== null)) {
+    //1. presignedUrl 생성요청
+    const imageData = {
+      ownerId: userId,
+      backgroundExtension: newBackgroundFileList?.[0].type.split('/')[1],
+      profileExtension: newProfileFileList?.[0].type.split('/')[1],
+    };
+    const response = await axiosInstance.post<UserPresignedUrlsType>('/users/upload-url', imageData);
+
+    //2. presignedUrl에 사진 업로드
+    const { backgroundPresignedUrl, profilePresignedUrl } = response?.data;
+
+    if (newBackgroundFileList !== null) {
+      const resultFile = await compressFile(newBackgroundFileList[0]);
+      await axios.put(backgroundPresignedUrl, resultFile);
+    }
+
+    if (newProfileFileList !== null) {
+      const resultFile = await compressFile(newProfileFileList[0]);
+      await axios.put(profilePresignedUrl, resultFile);
+    }
+
+    //3.서버에 성공 알림
+    await axiosInstance.post('/users/upload-complete', imageData);
+  }
+
+  return;
+};
+
+export default updateProfile;
