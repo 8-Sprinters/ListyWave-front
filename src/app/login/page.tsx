1d1dfd08ef61473d8cf2b222232bedd1dbc18ee4
ParkSohyunee <sohyunee016@gmail.com>
2024-02-14 15:43:22 +0900

    Design: 로그인 페이지 svg 파일 추가 및 페이지 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 /**
  TODO
  - [ ] 로그인 만료 확인, refreshToken(추후)
@@ -5,20 +7,43 @@
  */
 
 import Link from 'next/link';
+import Image from 'next/image';
+
+import NaverLoginIcon from '/public/icons/naver_login.svg';
+import GoogleLoginIcon from '/public/icons/google_login.svg';
+import KakaoLoginIcon from '/public/icons/kakao_login.svg';
 
 const oauthType = {
-  kakao: 'kakao',
   naver: 'naver',
   google: 'google',
+  kakao: 'kakao',
 };
+
+const baseUrl = 'https://dev.api.listywave.com'; // TODO 이 부분은 나중에 .env.local로 수정
 
 export default function LoginPage() {
   return (
-    <div>
-      로그인페이지
+    <section>
       <div>
-        <Link href={`https://dev.api.listywave.com/auth/${oauthType.kakao}`}>카카오 로그인</Link>
+        <Image src={`/icons/large_logo.svg`} width={207} height={35} alt="리스티웨이브 로고" priority />
+        <div>
+          <h1>시작하기</h1>
+          <p>
+            나만의 <span>리스트</span>를 만들어보세요!
+          </p>
+        </div>
       </div>
-    </div>
+      <div>
+        <Link href={`${baseUrl}/auth/${oauthType.naver}`}>
+          <NaverLoginIcon />
+        </Link>
+        <Link href={`${baseUrl}/auth/${oauthType.google}`}>
+          <GoogleLoginIcon />
+        </Link>
+        <Link href={`${baseUrl}/auth/${oauthType.kakao}`}>
+          <KakaoLoginIcon />
+        </Link>
+      </div>
+    </section>
   );
 }
