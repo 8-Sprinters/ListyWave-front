6022824409e04f6e49b2e1c38fcd0620ed54ea9d
Nahyun-Kang <tarea202001@gmail.com>
2024-02-15 14:51:20 +0900

    Fix: 사용자 추천 리스트 closeIcon 삭제, 추천 이유 삭제

--- src/components/exploreComponents/UsersRecommendation.tsx
+++ src/components/exploreComponents/UsersRecommendation.tsx
@@ -6,26 +6,15 @@ import { QUERY_KEYS } from '@/lib/constants/queryKeys';
 import getRecommendedUsers from '@/app/_api/explore/getRecommendedUsers';
 
 import * as styles from './UsersRecommendation.css';
-import { recommendationUsersMockdata } from './_mockdata/mockdata';
 import { UsersRecommendationType } from './_mockdata/mockdataType';
-import CloseButton from '/public/icons/close_x_gray.svg';
 
 function UsersRecommendation() {
-  const [recommendUsersList, setRecommendUserList] = useState<UsersRecommendationType[]>(recommendationUsersMockdata);
   const wrapperRef = useRef<HTMLUListElement>(null);
 
   const { data: usersList, isPending } = useQuery({
     queryKey: [QUERY_KEYS.getRecommendedUsers],
     queryFn: () => getRecommendedUsers(),
   });
-
-  const handleRemoveUser = (id: number) => {
-    if (!recommendUsersList) {
-      return null;
-    }
-    const list = recommendUsersList.filter((listItem) => listItem?.id !== id);
-    setRecommendUserList([...list]);
-  };
 
   const handleScrollToRight = () => {
     if (wrapperRef.current) {
@@ -38,18 +27,14 @@ function UsersRecommendation() {
 
   return (
     <div className={styles.wrapper}>
-      {recommendUsersList?.length !== 0 && (
+      {usersList?.length !== 0 && (
         <>
           <div className={styles.userRecommendationTitle}>사용자 추천</div>
           <ul className={styles.recommendUsersListWrapper} ref={wrapperRef}>
             {usersList?.map((item: UsersRecommendationType) => {
               return (
                 <li key={item.id}>
-                  <UserRecommendListItem
-                    data={item}
-                    handleRemoveUser={handleRemoveUser}
-                    handleScrollToRight={handleScrollToRight}
-                  />
+                  <UserRecommendListItem data={item} handleScrollToRight={handleScrollToRight} />
                 </li>
               );
             })}
@@ -64,11 +49,10 @@ export default UsersRecommendation;
 
 interface UserRecommendListItemProps {
   data: UsersRecommendationType;
-  handleRemoveUser: (id: number) => null | undefined;
   handleScrollToRight: () => void;
 }
 
-function UserRecommendListItem({ data, handleRemoveUser, handleScrollToRight }: UserRecommendListItemProps) {
+function UserRecommendListItem({ data, handleScrollToRight }: UserRecommendListItemProps) {
   const [isFollowing, setIsFollowing] = useState(false);
 
   const handleFollowingState = () => {
@@ -83,9 +67,6 @@ function UserRecommendListItem({ data, handleRemoveUser, handleScrollToRight }: 
   return (
     <>
       <div className={styles.recommendUserWrapper}>
-        <button onClick={() => handleRemoveUser(data.id)}>
-          <CloseButton width={24} height={24} alt="닫기 버튼" className={styles.closeButton} />
-        </button>
         <div className={styles.imageWrapper}>
           <Image
             src={data.profileImageUrl}
@@ -99,7 +80,6 @@ function UserRecommendListItem({ data, handleRemoveUser, handleScrollToRight }: 
           />
         </div>
         <h6 className={styles.recommendUserNickname}>{data.nickname}</h6>
-        <p className={styles.recommendUserDescription}>최근 활동한 사용자입니다.</p>
         <button
           className={`${styles.followButtonDefault} ${isFollowing === true ? styles.followButtonFollowing : ''}`}
           onClick={handleFollowButtonClick}
