7cdcb0e17652e9bf40f9c40be72e8e6bce43f137
ParkSohyunee <sohyunee016@gmail.com>
2024-02-17 17:17:03 +0900

    Feat: 리스트 작성페이지 이동시 권한 체크 및 로그인 모달 연동

--- src/components/floatingButton/PlusOptionFloatingButton.tsx
+++ src/components/floatingButton/PlusOptionFloatingButton.tsx
@@ -2,7 +2,7 @@
 
 /**
  TODO
- - [ ] 클릭 시 옵션 메뉴 구현
+ - [x] 클릭 시 옵션 메뉴 구현
  - [ ] 다른 페이지에서도 사용할 수 있도록 공통 컴포넌트화(리팩토링)
  */
 
@@ -19,30 +19,45 @@ import toasting from '@/lib/utils/toasting';
 import toastMessage from '@/lib/constants/toastMessage';
 import { useUser } from '@/store/useUser';
 
+import LoginModal from '@/components/login/LoginModal';
+import Modal from '@/components/Modal/Modal';
+
 function FloatingMenu() {
   const router = useRouter();
   const { user } = useUser();
+  const { isOn, handleSetOn, handleSetOff } = useBooleanOutput();
+
+  const handleSharePage = () => {
+    // TODO 카카오 공유하기 기능
+  };
 
   const handleMoveToPage = () => {
     // TODO 토큰 유효성 검증 관련 인가 처리 로직 추가
     // 지금은 토큰이 있는 지 확인 후 이동만 간단하게 처리시켜 둠(추후 수정 예정)
     if (!user.id) {
       toasting({ type: 'warning', txt: toastMessage.ko.requiredLogin });
-      // TODO 로그인 모달
+      handleSetOn();
       return;
     }
     router.push('/list/create');
   };
 
   return (
-    <div className={styles.menuButtons}>
-      <div className={styles.addButton}>
-        <ShareAltIcon alt="내 피드 공유하기 버튼" className={styles.icon} />
+    <>
+      <div className={styles.menuButtons}>
+        <div className={styles.addButton} onClick={handleSharePage}>
+          <ShareAltIcon alt="내 피드 공유하기 버튼" className={styles.icon} />
+        </div>
+        <div className={styles.addButton} onClick={handleMoveToPage}>
+          <WriteIcon alt="리스트 작성하기 버튼" className={styles.icon} />
+        </div>
       </div>
-      <div className={styles.addButton} onClick={handleMoveToPage}>
-        <WriteIcon alt="리스트 작성하기 버튼" className={styles.icon} />
-      </div>
-    </div>
+      {isOn && (
+        <Modal handleModalClose={handleSetOff} size="large">
+          <LoginModal />
+        </Modal>
+      )}
+    </>
   );
 }
 
