7399ed5cec85e333e488a9edcb719787ff283e71
Eugene-A-01 <eugene01@yonsei.ac.kr>
2024-02-18 18:56:30 +0900

    Fix: 파라미터 타입 수정

--- src/app/_api/follow/getFollowingList.ts
+++ src/app/_api/follow/getFollowingList.ts
@@ -1,7 +1,7 @@
 import axiosInstance from '@/lib/axios/axiosInstance';
 import { FollowingListType } from '@/lib/types/followType';
 
-const getFollowingList = async (userId: number) => {
+const getFollowingList = async (userId: number | null) => {
   const response = await axiosInstance.get<FollowingListType>(`users/${userId}/followings`);
 
   return response.data;
