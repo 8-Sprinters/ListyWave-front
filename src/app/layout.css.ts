eefe3bee927609ee41dc6aa49142dae950d03cad
Seoyoung <seoyoungiyo@gmail.com>
2024-02-20 18:40:43 +0900

    Refactor: 430 기준 더 이상 늘어나지 않도록 레이아웃 수정

--- src/app/layout.css.ts
+++ src/app/layout.css.ts
@@ -1,5 +1,23 @@
+import { vars } from '@/styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
-export const wrapper = style({
+export const body = style({
+  width: '100%',
+  height: '100%',
+
   position: 'relative',
+
+  backgroundColor: vars.color.gray3,
+});
+
+export const main = style({
+  minHeight: '100vh',
+  maxWidth: 430,
+  width: '100%',
+  height: '100%',
+  margin: 'auto',
+
+  position: 'relative',
+
+  backgroundColor: vars.color.white,
 });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,10 @@ import { ReactNode } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Script from 'next/script';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import '@/styles/GlobalStyles.css';
 import BottomNav from '@/components/BottomNav/BottomNav';
+import '@/styles/GlobalStyles.css';
+
+import * as styles from './layout.css';
 
 const queryClient = new QueryClient();
 declare global {
@@ -32,10 +34,10 @@ export default function TempLayout({ children }: { children: ReactNode }) {
           strategy="lazyOnload"
         />
       </head>
-      <body>
+      <body className={styles.body}>
         <QueryClientProvider client={queryClient}>
           <div id="modal-root" />
-          <div>
+          <div className={styles.main}>
             {children}
             <BottomNav />
           </div>
