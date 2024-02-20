b44c0ab901790a8f05c2517b53a07967f28815fa
Seoyoung <seoyoungiyo@gmail.com>
2024-02-20 20:06:36 +0900

    Design: 모달 버튼 색상 변경으로 새로운 색 추가 및 모달 버튼 색상 변경

--- src/components/Modal/Modal.css.ts
+++ src/components/Modal/Modal.css.ts
@@ -2,13 +2,13 @@ import { style, styleVariants, ComplexStyleRule } from '@vanilla-extract/css';
 import { vars } from '@/styles/theme.css';
 
 export const background = style({
-  width: '100vw',
+  maxWidth: 430,
+  margin: 'auto',
+  width: '100vh',
   height: '100vh',
   zIndex: 100,
 
-  position: 'fixed',
-  top: '0px',
-  left: '0px',
+  position: 'absolute',
 
   display: 'flex',
   justifyContent: 'center',
