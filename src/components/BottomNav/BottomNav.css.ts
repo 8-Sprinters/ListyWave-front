5c15c45be015b0efe8d533e9f500358e90f7b13b
Seoyoung <seoyoungiyo@gmail.com>
2024-02-20 18:44:59 +0900

    Refactor: 최대 430에 맞게 스타일 일부 변경

--- src/components/BottomNav/BottomNav.css.ts
+++ src/components/BottomNav/BottomNav.css.ts
@@ -2,17 +2,18 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '@/styles/theme.css';
 
 export const navDiv = style({
+  margin: 'auto',
+  maxWidth: 430,
   width: '100%',
   height: 70,
 
   position: 'fixed',
   bottom: 0,
-  zIndex: 10,
 
   display: 'flex',
 
   backgroundColor: vars.color.white,
-  border: `1px solid ${vars.color.gray5}`,
+  borderTop: `1px solid ${vars.color.gray5}`,
 });
 
 export const buttonDiv = style({
