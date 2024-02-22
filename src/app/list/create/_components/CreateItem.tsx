6f036ac99f5debf607ca2ba3dbb2d5e507afebac
Eugene-A-01 <eugene01@yonsei.ac.kr>
2024-02-22 18:22:55 +0900

    Feat: 이미지의 수정이 있을때에만 presigned url 요청하도록 로직 구현

--- src/app/list/create/_components/CreateItem.tsx
+++ src/app/list/create/_components/CreateItem.tsx
@@ -9,9 +9,16 @@ interface CreateItemProps {
   onSubmitClick: () => void;
   isSubmitting: boolean;
   type: 'create' | 'edit';
+  setItemChanged?: () => void;
 }
 
-export default function CreateItem({ onBackClick, onSubmitClick, isSubmitting, type }: CreateItemProps) {
+export default function CreateItem({
+  onBackClick,
+  onSubmitClick,
+  isSubmitting,
+  type,
+  setItemChanged,
+}: CreateItemProps) {
   const {
     formState: { isValid },
   } = useFormContext();
@@ -41,7 +48,7 @@ export default function CreateItem({ onBackClick, onSubmitClick, isSubmitting, t
           최소 3개, 최대 10개까지 아이템을 추가할 수 있어요. <br />
           아이템의 순서대로 순위가 정해져요.
         </p>
-        <Items disabled={type === 'edit'} />
+        <Items type={type} setItemChanged={setItemChanged} />
       </div>
     </div>
   );
