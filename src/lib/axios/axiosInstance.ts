8f2f3242d306a9451c8dbc677596d7d27fc4a255
ParkSohyunee <sohyunee016@gmail.com>
2024-02-22 21:26:23 +0900

    Feat: 쿠키에 저장된 토큰 request 요청에 포함되도록 axios 설정

--- src/lib/axios/axiosInstance.ts
+++ src/lib/axios/axiosInstance.ts
@@ -1,5 +1,6 @@
-import axios from 'axios';
-import { useUser } from '@/store/useUser';
+import axios, { AxiosRequestConfig } from 'axios';
+// import { useUser } from '@/store/useUser';
+import { getCookie } from '../utils/cookie';
 
 const axiosInstance = axios.create({
   baseURL: 'https://dev.api.listywave.com',
@@ -8,7 +9,11 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const accessToken = useUser.getState().user.accessToken;
+    // zustande persist에 저장된 토큰 꺼내쓰기 버전 - 방법 1
+    // const accessToken = useUser.getState().user.accessToken;
+
+    // cookie에 저장된 토큰 꺼내쓰기 버전 - 방법 2
+    const accessToken = getCookie('accessToken');
 
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
@@ -21,5 +26,29 @@ axiosInstance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+// interceptors 쿠키로 보내기 버전 - 방법 1
+// axiosInstance.interceptors.response.use(
+//   (res) => res,
+//   async (error) => {
+//     const originalRequest = error.config as AxiosRequestConfig & { _retry?: boolean };
+//     if (error.response?.status === 401 && !originalRequest._retry) {
+//       const response = await axiosInstance.get('/auth/token', {
+//         _retry: true,
+//         withCredentials: true,
+//       });
+//       console.log('여기');
+
+//       console.log(response.data);
+
+//       useUser.getState().updateUser({ id: 13, accessToken: response.data.accessToken });
+
+//       originalRequest._retry = true;
+//       axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${response.data.accessToken}`;
+//       return axiosInstance(originalRequest);
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosInstance;
