52afcdd99bb2b0b9d8c6d214b6ef27b79e977a51
Seoyoung <seoyoungiyo@gmail.com>
2024-02-25 23:20:27 +0900

    Design: 스켈레톤 디자인 수정

--- src/app/notification/_components/NotificationListSkeleton.tsx
+++ src/app/notification/_components/NotificationListSkeleton.tsx
@@ -4,12 +4,12 @@ import * as styles from './NotificationList.css';
 export default function NotificationListSkeleton() {
   return (
     <div className={styles.list}>
-      {Array.from({ length: 5 }).map((_, index) => (
+      {Array.from({ length: 7 }).map((_, index) => (
         <div key={index} className={styles.notification}>
           <Skeleton variant="circular" width={50} height={50} animation="wave" />
           <div className={styles.baseMessage}>
-            <Skeleton variant="text" animation="wave" />
-            <Skeleton variant="text" animation="wave" width={150} height={20} />
+            <Skeleton variant="text" animation="wave" width={270} height={20} />
+            <Skeleton variant="text" animation="wave" width={200} height={20} />
           </div>
         </div>
       ))}
