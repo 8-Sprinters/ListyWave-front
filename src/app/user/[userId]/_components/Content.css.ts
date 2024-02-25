33a015b3798e2a4ad0351c725688e70d484f4b50
ParkSohyunee <sohyunee016@gmail.com>
2024-02-25 14:59:33 +0900

    Design: 피드페이지 디자인 일부 수정 - 호버시 박스쉐도우 추가 - nodata 이미지가 보일시 높이 수정

--- src/app/user/[userId]/_components/Content.css.ts
+++ src/app/user/[userId]/_components/Content.css.ts
@@ -67,6 +67,10 @@ export const cards = style({
   textAlign: 'center',
 });
 
+export const nodataContainer = style({
+  minHeight: '250px',
+});
+
 export const target = style({
   width: '100%',
   height: '2px',
