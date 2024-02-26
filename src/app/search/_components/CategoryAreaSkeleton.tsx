58d538b1243d6a0704aae5aa59df160d83514736
kanglocal <develocal25@gmail.com>
2024-02-26 18:24:47 +0900

    Fix : CategoryAreaSkeleton 수정

--- src/app/search/_components/CategoryAreaSkeleton.tsx
+++ src/app/search/_components/CategoryAreaSkeleton.tsx
@@ -1,15 +1,12 @@
 import Skeleton from '@mui/material/Skeleton';
 import * as styles from './CategoryArea.css';
 
-// TODO: 임시 UI
 function CategoryAreaSkeleton() {
   return (
-    <div className={styles.categoryWrapper}>
-      <div className={styles.category}>
-        <Skeleton variant="rounded" width="6rem" height="6rem" animation="wave" />
-        <div className={styles.categoryText}>
-          <Skeleton variant="text" width="6rem" height="2rem" animation="wave" />
-        </div>
+    <div className={styles.category}>
+      <Skeleton variant="rounded" width="6rem" height="6rem" animation="wave" />
+      <div className={styles.categoryText}>
+        <Skeleton variant="text" width="6rem" height="2rem" animation="wave" />
       </div>
     </div>
   );
