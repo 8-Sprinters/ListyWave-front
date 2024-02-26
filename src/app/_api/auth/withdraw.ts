2d238df48a6aa4b11ce2f3b0e1446d5eb05adcc6
Seoyoung <seoyoungiyo@gmail.com>
2024-02-26 21:46:52 +0900

    Fix: 잘못된 endpoint 수정

--- src/app/_api/auth/withdraw.ts
+++ src/app/_api/auth/withdraw.ts
@@ -2,7 +2,7 @@ import axiosInstance from '@/lib/axios/axiosInstance';
 
 //회원 탈퇴 api
 const withdraw = async () => {
-  await axiosInstance.delete(`/auth/withdraw`);
+  await axiosInstance.delete(`/withdraw`);
 };
 
 export default withdraw;
