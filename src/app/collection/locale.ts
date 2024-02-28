5ccf9d0b207dee242a989bb7888bf40dba03f92f
seoyoung-min <seoyoungiyo@gmail.com>
2024-02-28 17:22:06 +0900

    Fix: collection title (#163)
    * Fix: 콜렉션 카테고리  페이지 타이틀 변경
    
    * Chore: fallback 이미지 추가

--- src/app/collection/locale.ts
+++ src/app/collection/locale.ts
@@ -12,3 +12,30 @@ export const collectionLocale = {
     collection: 'Collection',
   },
 };
+
+export const categoriesLocale = {
+  ko: {
+    entire: '전체',
+    culture: '문화',
+    life: '일상생활',
+    place: '장소',
+    music: '음악',
+    movie_drama: '영화/드라마',
+    book: '도서',
+    animal_plant: '동식물',
+    etc: '기타',
+    food: '음식',
+  },
+  en: {
+    entire: 'Entire',
+    culture: 'Culture',
+    life: 'Life',
+    place: 'Place',
+    music: 'Music',
+    movie_drama: 'Movie&Drama',
+    book: 'Book',
+    animal_plant: 'Animal&Plant',
+    etc: 'Etc.',
+    food: 'Food',
+  },
+};
