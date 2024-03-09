4487cd30147b2536ec3e209a1ba5bda2af14f9fe
ParkSohyunee <sohyunee016@gmail.com>
2024-03-09 16:23:41 +0900

    Style: 불필요한 파일 삭제, 일부 컴포넌트 컨벤션 수정

--- src/app/user/[userId]/_components/SkeletonDesign.css.ts
+++
@@ -1,8 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const gridContainer = style({
-  display: 'grid',
-  gridTemplateColumns: 'repeat(2, 1fr)',
-  rowGap: '1.6rem',
-  columnGap: '1.2rem',
-});
