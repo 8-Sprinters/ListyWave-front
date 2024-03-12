27f40d267401736eb315dc001bb3aba37f561adc
Seoyoung <seoyoungiyo@gmail.com>
2024-03-12 23:23:57 +0900

    Chore: 환경 변수 설정

--- src/lib/utils/gtag.tsx
+++ src/lib/utils/gtag.tsx
@@ -12,12 +12,13 @@ export const event = ({ action, parameters }: any) => {
   window.gtag('event', action, parameters);
 };
 
+const GTM_CONTAINER_ID = process.env.NEXT_PUBLIC_GTM_ID;
 //Google Tag Manager
 export const GtmScript = () => {
   return (
     <script
       dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T36W9G3V');`,
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');`,
       }}
     />
   );
@@ -28,7 +29,7 @@ export const GtmNoScript = () => {
   return (
     <noscript>
       <iframe
-        src="https://www.googletagmanager.com/ns.html?id=GTM-T36W9G3V"
+        src={`https://www.googletagmanager.com/ns.html?id=${GTM_CONTAINER_ID}`}
         height="0"
         width="0"
         style={{ display: 'none', visibility: 'hidden' }}
