7709b2afe1cc72e988a0356bd7bc7c8a212edfc2
Eugene-A-01 <eugene01@yonsei.ac.kr>
2024-04-30 20:11:14 +0900

    Chore: PWA 개발용 워크박스 콘솔 출력 없앰

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
 });
 
 /** @type {import('next').NextConfig} */
