100991b5e4a22caa7b92423993377bea5f756c63
Eugene Ahn <70089733+Eugene-A-01@users.noreply.github.com>
2024-05-15 21:55:49 +0900

    ListyWave ver2.4.0 : 리스트 배경색 확장 및 팔레트 도입 (#239)
    * Feat: 리스트 배경색 확장
    
    * Feat: 리스트배경색 팔레트 구현
    
    * Feat: hex원시값이 아닌 컬러코드로 통신하도록 API 수정 및 반영
    
    * Feat: 트렌딩리스트 배경색에도 변경된 컬러 API 반영

---
+++ src/app/list/create/_components/list/PaletteSelector.css.ts
@@ -0,0 +1,11 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  width: '100%',
+
+  padding: '0 30px 5px 40px',
+
+  display: 'flex',
+  justifyContent: 'flex-end',
+  alignItems: 'center',
+});
