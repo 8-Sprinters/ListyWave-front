35dd8e5535412d1228e9905f5a8c1801deb3bf39
ParkSohyunee <sohyunee016@gmail.com>
2024-05-18 16:57:08 +0900

    Feat: 마이리스트, 콜라보리스트 페이지에 Dynamic 메타데이터 설정

--- src/app/user/[userId]/collabolist/page.tsx
+++ src/app/user/[userId]/collabolist/page.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 
 import Profile from '../_components/Profile';
 import Content from '../_components/Content';
@@ -6,16 +6,30 @@ import FloatingContainer from '@/components/floatingButton/FloatingContainer';
 import PlusOptionFloatingButton from '@/components/floatingButton/PlusOptionFloatingButton';
 import ArrowUpFloatingButton from '@/components/floatingButton/ArrowUpFloatingButton';
 
+import axiosInstance from '@/lib/axios/axiosInstance';
+import { UserType } from '@/lib/types/userProfileType';
+
 interface CollaboListPageProps {
-  params: {
-    userId: number;
-  };
+  params: { userId: number };
 }
 
-export const metadata: Metadata = {
-  title: 'Collabo List',
-  description: '콜라보레이터와 함께 기록한 리스트 입니다.',
-};
+export async function generateMetadata({ params }: CollaboListPageProps, parent: ResolvingMetadata): Promise<Metadata> {
+  const userId = params.userId;
+  const { data } = await axiosInstance.get<UserType>(`/users/${userId}`);
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: {
+      absolute: `${data.nickname}'s CollaboList`,
+    },
+    description: `${data.nickname}님의 콜라보레이터와 함께 기록한 리스트 입니다.`,
+    openGraph: {
+      description: `${data.description ? data.description : `${data.nickname}님의 콜라보레이터와 함께 기록한 리스트 입니다.`}`,
+      images: [`${data.profileImageUrl}`, ...previousImages],
+    },
+  };
+}
 
 export default function CollaboListPage({ params }: CollaboListPageProps) {
   return (
--- src/app/user/[userId]/mylist/page.tsx
+++ src/app/user/[userId]/mylist/page.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from 'next';
+import { Metadata, ResolvingMetadata } from 'next';
 
 import Profile from '../_components/Profile';
 import Content from '../_components/Content';
@@ -6,16 +6,30 @@ import FloatingContainer from '@/components/floatingButton/FloatingContainer';
 import PlusOptionFloatingButton from '@/components/floatingButton/PlusOptionFloatingButton';
 import ArrowUpFloatingButton from '@/components/floatingButton/ArrowUpFloatingButton';
 
+import axiosInstance from '@/lib/axios/axiosInstance';
+import { UserType } from '@/lib/types/userProfileType';
+
 interface MyListPageProps {
-  params: {
-    userId: number;
-  };
+  params: { userId: number };
 }
 
-export const metadata: Metadata = {
-  title: 'My List',
-  description: '나의 취향을 기록한 나만의 리스트 입니다.',
-};
+export async function generateMetadata({ params }: MyListPageProps, parent: ResolvingMetadata): Promise<Metadata> {
+  const userId = params.userId;
+  const { data } = await axiosInstance.get<UserType>(`/users/${userId}`);
+
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: {
+      absolute: `${data.nickname}'s Mylist`,
+    },
+    description: `${data.nickname}님의 취향을 기록한 리스트입니다.`,
+    openGraph: {
+      description: `${data.description ? data.description : `${data.nickname}님의 취향을 기록한 리스트입니다.`}`,
+      images: [`${data.profileImageUrl}`, ...previousImages],
+    },
+  };
+}
 
 export default function MyListPage({ params }: MyListPageProps) {
   return (
