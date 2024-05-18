b2865eab70d61d856b73ddcbd7480dc631709061
ParkSohyunee <sohyunee016@gmail.com>
2024-05-18 16:02:23 +0900

    feat: layout 컴포넌트의 metadata 수정 - 리스티웨이브 한글 타이틀 추가 및 description 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,20 +22,20 @@ export const metadata: Metadata = {
   //  Template Object
   title: {
     template: '%s | ListyWave',
-    default: 'ListyWave', // 대체 제목 (required),
+    default: 'ListyWave | 리스티웨이브', // 대체 제목 (required),
   },
   description:
-    "What’s In Your List? 🌊 나의 취향을 리스트로 기록하고, 공유하고, 발견해요. 리스티웨이브에서 모든 기준은 '나의 취향'이에요. 내 취향 가득한 편안한 공간이 되면 좋겠습니다.",
+    "나의 취향을 리스트로 기록하고, 공유하고, 발견해요. 리스티웨이브에서 모든 기준은 '나의 취향'이에요. 내 취향 가득한 편안한 공간이 되면 좋겠습니다.",
   authors: [{ name: '에잇🩷' }],
   generator: 'Next.js',
   applicationName: 'ListyWave',
   referrer: 'origin-when-cross-origin', // Referrer-Policy
-  keywords: ['ListyWave', 'list', 'SNS'],
+  keywords: ['ListyWave', 'list', '리스티웨이브'],
   metadataBase: new URL('https://listywave.com'),
   openGraph: {
     title: 'ListyWave',
     description:
-      "What’s In Your List? 🌊 나의 취향을 리스트로 기록하고, 공유하고, 발견해요. 리스티웨이브에서 모든 기준은 '나의 취향'이에요. 내 취향 가득한 편안한 공간이 되면 좋겠습니다.",
+      "나의 취향을 리스트로 기록하고, 공유하고, 발견해요. 리스티웨이브에서 모든 기준은 '나의 취향'이에요. 내 취향 가득한 편안한 공간이 되면 좋겠습니다.",
     url: 'https://listywave.com',
     type: 'website',
     siteName: 'ListyWave',
