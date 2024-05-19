e564f4631c646f407f0e4905d62fb77a643ed100
ParkSohyunee <sohyunee016@gmail.com>
2024-05-19 19:39:55 +0900

    Feat: 트렌딩리스트에 해당하는 리스트의 상세페이지가 sitemap에 추가되도록 설정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,17 @@
 import type { MetadataRoute } from 'next';
 import DOMAIN_URL from '@/lib/constants/domain';
+import getTrendingLists from './_api/explore/getTrendingLists';
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const result = await getTrendingLists();
+
+  const trendingLists: MetadataRoute.Sitemap = result.map((list) => ({
+    url: `${DOMAIN_URL}/list/${list.id}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 0.6,
+  }));
+
   return [
     {
       url: DOMAIN_URL,
@@ -21,5 +31,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.8,
     },
+    ...trendingLists,
   ];
 }
