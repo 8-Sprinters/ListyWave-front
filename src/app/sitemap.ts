8e641307e9cc6d2f45bb8a62024e9c9f4323f915
ParkSohyunee <sohyunee016@gmail.com>
2024-06-24 22:43:04 +0900

    Fix: sitemap.ts 트랜딩리스트 호출 로직 제거

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,20 +1,7 @@
 import type { MetadataRoute } from 'next';
 import DOMAIN_URL from '@/lib/constants/domain';
-// import getTrendingLists from './_api/explore/getTrendingLists';
-import { TrendingListType } from '@/lib/types/exploreType';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const result: TrendingListType[] = await fetch(`${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/lists/explore`).then((res) =>
-    res.json()
-  );
-
-  const trendingLists: MetadataRoute.Sitemap = result.map((list) => ({
-    url: `${DOMAIN_URL}/list/${list.id}`,
-    lastModified: new Date(),
-    changeFrequency: 'weekly',
-    priority: 0.6,
-  }));
-
   return [
     {
       url: DOMAIN_URL,
@@ -34,6 +21,5 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'weekly',
       priority: 0.8,
     },
-    ...trendingLists,
   ];
 }
