9835032e35758f549318479585c698e463a7278d
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-09-02 13:30:54 +0900

    Feat: 리스트 공개, 비공개 상태에 따른 팝업메뉴 text 적용

--- src/app/user/[userId]/_components/OptionToggleButton.tsx
+++ src/app/user/[userId]/_components/OptionToggleButton.tsx
@@ -6,10 +6,16 @@ import OptionMenuIcon from '/public/icons/popup_menu.svg';
 import useOnClickOutside from '@/hooks/useOnClickOutside';
 import useBooleanOutput from '@/hooks/useBooleanOutput';
 
+interface OptionToggleButton {
+  isPublicCurrent: boolean;
+}
+
 // memoization
-export default function OptionToggleButton() {
+export default function OptionToggleButton({ isPublicCurrent }: OptionToggleButton) {
   const { isOn: isPopupOpen, toggle: popupToggle, handleSetOff: handlePopupOff } = useBooleanOutput();
   const { ref: popupRef } = useOnClickOutside(handlePopupOff);
+
+  const publicAction = isPublicCurrent ? '비공개' : '공개';
 
   const handleOpenMenu = (e: MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
@@ -26,7 +32,7 @@ export default function OptionToggleButton() {
       <OptionMenuIcon alt="리스트 공개, 비공개 옵션" />
       {isPopupOpen && (
         <button className={styles.optionMenu} onClick={handleToggleVisibilityList}>
-          비공개
+          {publicAction}
         </button>
       )}
     </div>
