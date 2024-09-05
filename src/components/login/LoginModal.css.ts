6b55f9ecd8f402d1032d7cf08de233f4abd7aeaa
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-09-05 14:11:22 +0900

    Feat: 로그인 모달에 로컬용 로그인 기능 추가

--- src/components/login/LoginModal.css.ts
+++ src/components/login/LoginModal.css.ts
@@ -46,12 +46,22 @@ export const title = style([
 ]);
 
 export const buttonContainer = style({
-  padding: '2.4rem 2.8rem 0 2.8rem',
+  width: '100%',
+  paddingTop: '2.4rem',
 
   display: 'flex',
-  justifyContent: 'space-between',
+  justifyContent: 'center',
   alignItems: 'center',
-  gap: '1.8rem',
+  gap: '1rem',
 
   borderTop: `1px solid ${vars.color.gray5}`,
+});
+
+export const buttonForLocal = style({
+  height: '4.7rem',
+  padding: '0.2rem',
+
+  border: `1px solid ${vars.color.gray5}`,
+  background: vars.color.lightblue,
+  borderRadius: '1.2rem',
 });
