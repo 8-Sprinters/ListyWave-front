8dba1a4c681fe7abe0890f1fe20465552ed2d82d
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-09-06 13:05:58 +0900

    Feat: 네브 탭 아이콘 추가 및 하드코딩 된 path를 객체로 리팩토링

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -3,8 +3,9 @@
 import { usePathname, useRouter } from 'next/navigation';
 import Link from 'next/link';
 
-import HomeIcon from '/public/icons/explore.svg';
-import MyFeedIcon from '/public/icons/my_feed.svg';
+import HomeIcon from '/public/icons/new/bottom_nav_home.svg';
+import AddIcon from '/public/icons/new/bottom_nav_add.svg';
+import MyFeedIcon from '/public/icons/new/bottom_nav_feed.svg';
 
 import useBooleanOutput from '@/hooks/useBooleanOutput';
 import { useUser } from '@/store/useUser';
@@ -15,16 +16,30 @@ import * as styles from './BottomNav.css';
 import Modal from '@/components/Modal/Modal';
 import LoginModal from '@/components/login/LoginModal';
 
+type BottomNavTapPathType = 'home' | 'feed' | 'list';
+
+const home = 'home';
+const feed = 'my-feed';
+const list = 'create-list';
+
+const bottomNavTapPath: Record<BottomNavTapPathType, string> = {
+  home,
+  feed,
+  list,
+};
+
+// TODO 하드코딩된 path를 줄이는 방법이 없을까?
+
 export default function BottomNav() {
   const { user } = useUser();
   const router = useRouter();
-  const pathname = usePathname() as string;
+  const pathname = usePathname();
   const { isOn, handleSetOff, handleSetOn } = useBooleanOutput();
 
   const userId = user.id;
 
   const visiblePaths = ['/', '/mylist', 'collection'];
-  const isVisible = visiblePaths.some((path) => pathname.endsWith(path));
+  const isVisible = visiblePaths.some((path) => pathname?.endsWith(path));
 
   if (!isVisible) {
     return <></>;
@@ -34,34 +49,37 @@ export default function BottomNav() {
   const selectedItem = (() => {
     switch (pathname) {
       case '/':
-        return 'home';
+        return bottomNavTapPath.home;
       case `/user/${userId}/mylist`:
-        return 'my-feed';
+        return bottomNavTapPath.feed;
       default:
         return null;
     }
   })();
 
   // 로그인한 사용자 검증 후, 마이피드로 이동
-  const handleMoveToPageOnLogin = () => {
+  const handleMoveToPageOnLogin = (path: string) => () => {
     if (!userId) {
       handleSetOn();
       return;
     }
-    router.push(`/user/${userId}/mylist`);
+    path === bottomNavTapPath.feed ? router.push(`/user/${userId}/mylist`) : router.push('/list/create');
   };
 
   return (
     <>
       <nav>
-        <ul className={styles.navDiv}>
+        <div className={styles.navDiv}>
           <Link href="/" className={styles.buttonDiv}>
-            <HomeIcon fill={selectedItem === 'home' ? vars.color.blue : vars.color.gray7} />
+            <HomeIcon fill={selectedItem === bottomNavTapPath.home ? vars.color.blue : vars.color.gray7} />
           </Link>
-          <li className={styles.buttonDiv} onClick={handleMoveToPageOnLogin}>
-            <MyFeedIcon fill={selectedItem === 'my-feed' ? vars.color.blue : vars.color.gray7} />
-          </li>
-        </ul>
+          <button className={styles.buttonDiv} onClick={handleMoveToPageOnLogin(bottomNavTapPath.list)}>
+            <AddIcon fill={selectedItem === bottomNavTapPath.list ? vars.color.blue : vars.color.gray7} />
+          </button>
+          <button className={styles.buttonDiv} onClick={handleMoveToPageOnLogin(bottomNavTapPath.feed)}>
+            <MyFeedIcon fill={selectedItem === bottomNavTapPath.feed ? vars.color.blue : vars.color.gray7} />
+          </button>
+        </div>
       </nav>
 
       {isOn && (
