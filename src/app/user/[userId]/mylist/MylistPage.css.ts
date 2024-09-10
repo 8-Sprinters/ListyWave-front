cfcbe7d35ec08de8d9ec9f280268797e9aacdced
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-09-10 14:49:32 +0900

    Design: 리스트 스크롤 영역 높이가 상하단 요소 크기에 맞춰 조정될 수 있도록 적용

--- src/app/user/[userId]/mylist/MylistPage.css.ts
+++ src/app/user/[userId]/mylist/MylistPage.css.ts
@@ -3,6 +3,9 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '@/styles/theme.css';
 
 export const container = style({
-  // height: '100vh',
+  width: '100%',
+  height: '100%',
+
+  position: 'fixed',
   backgroundColor: vars.color.bgblue,
 });
