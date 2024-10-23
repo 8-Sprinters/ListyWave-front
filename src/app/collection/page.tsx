1d9de40c74bdc5943bb65c8c02b0f219ca26fd93
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-10-24 00:52:33 +0900

    Feat: 콜렉션 폴더 Link 태그로 변경 및 href 설정

--- src/app/collection/page.tsx
+++ src/app/collection/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useQuery } from '@tanstack/react-query';
@@ -28,7 +29,7 @@ export default function CollectionPage() {
       <div className={styles.container}>
         <div className={styles.folders}>
           {data?.folders.map((folder) => (
-            <div key={folder.folderId} className={styles.folder}>
+            <Link href={`/collection/${folder.folderId}`} key={folder.folderId} className={styles.folder}>
               <div className={styles.folderShape}>
                 <div className={styles.topLeftShape}></div>
                 <div className={styles.topShape}></div>
@@ -38,7 +39,7 @@ export default function CollectionPage() {
                 <span className={styles.folderName}>{folder.folderName}</span>
                 <span>{`(${folder.listCount})`}</span>
               </p>
-            </div>
+            </Link>
           ))}
         </div>
         <div className={styles.addFolderButtonContainer}>
