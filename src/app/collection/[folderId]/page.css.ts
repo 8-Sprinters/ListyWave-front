9a60d061aaf88f67bc6bc3383b83d782d7185a05
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-10-24 13:22:48 +0900

    Design: 콜렉션 상세페이지 퍼블리싱

---
+++ src/app/collection/[folderId]/page.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '@/styles/theme.css';
+
+export const container = style({
+  height: '100vh',
+  backgroundColor: vars.color.bggray,
+});
