0c9a5a42e04fdee019f7fbe15741ffd32e419f7c
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-10-24 15:09:34 +0900

    Design: 배경이미지 유무에 따른 스타일 variants 적용

--- src/app/collection/[folderId]/_components/Collections.tsx
+++ src/app/collection/[folderId]/_components/Collections.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { useQuery } from '@tanstack/react-query';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
 
 import * as styles from './Collections.css';
 
@@ -25,24 +26,34 @@ export default function Collections({ folderId }: CollectionsProps) {
 
   return (
     <ul className={styles.container}>
-      {data?.collectionLists.map(({ list, id }) => (
-        <Link key={id} href={`/list/${list.id}`} className={styles.content}>
-          <div className={styles.category}>{list.category}</div>
-          <div className={styles.info}>
-            <h3 className={styles.title}>{list.title}</h3>
-            <p className={styles.owner}>Sehui Park</p>
-          </div>
-          <ul className={styles.items}>
-            {list.listItems.map((item) => (
-              <li key={item.id} className={styles.item}>
-                <span>{item.rank}</span>
-                <span>{item.title}</span>
-              </li>
-            ))}
-          </ul>
-          <p className={styles.date}>{formatDate(list.updatedDate)}</p>
-        </Link>
-      ))}
+      {data?.collectionLists.map(({ list, id }) => {
+        const hasImage = !!list.representativeImageUrl;
+        return (
+          <Link
+            key={id}
+            href={`/list/${list.id}`}
+            className={styles.contentVariant[hasImage ? 'round' : 'square']}
+            style={assignInlineVars({
+              [styles.imageUrl]: `url(${hasImage ? list.representativeImageUrl : null})`,
+            })}
+          >
+            <div className={styles.category}>{list.category}</div>
+            <div className={styles.info}>
+              <h3 className={styles.title[hasImage ? 'white' : 'black']}>{list.title}</h3>
+              <p className={styles.owner[hasImage ? 'white' : 'black']}>Sehui Park</p>
+            </div>
+            <ul className={styles.items}>
+              {list.listItems.map((item) => (
+                <li key={item.id} className={styles.itemVariant[hasImage ? 'blue' : 'white']}>
+                  <span>{item.rank}</span>
+                  <span>{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <p className={styles.date[hasImage ? 'white' : 'black']}>{formatDate(list.updatedDate)}</p>
+          </Link>
+        );
+      })}
     </ul>
   );
 }
