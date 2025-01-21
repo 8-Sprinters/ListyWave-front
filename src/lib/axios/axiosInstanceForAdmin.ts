3796a30069bcc9c55162aedd0b93e901dabe9c7d
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2025-01-21 15:32:52 +0900

    Feat: accessToken 만료 시, refreshToken으로 토큰 재발급 받는 로직 구현

--- src/lib/axios/axiosInstanceForAdmin.ts
+++ src/lib/axios/axiosInstanceForAdmin.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getCookie } from '../utils/cookie';
+import { getCookie, removeCookie, setCookie } from '../utils/cookie';
 
 const axiosInstanceForAdmin = axios.create({
   baseURL: process.env.NEXT_PUBLIC_SERVER_DOMAIN,
@@ -17,6 +17,53 @@ axiosInstanceForAdmin.interceptors.request.use(
   },
   (error) => {
     console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+let isRefreshing = false;
+
+axiosInstanceForAdmin.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+    const refreshToken = getCookie('admin-refreshToken');
+
+    if (error.response?.status === 401 && error.response?.data.error === 'UNAUTHORIZED') {
+      if (!isRefreshing && refreshToken === undefined) {
+        // accessToken 만료되었는데, refreshToken 없는 경우, storage 비우기
+        removeCookie('admin-accessToken');
+        removeCookie('admin-refreshToken');
+
+        isRefreshing = true;
+      }
+
+      if (!isRefreshing) {
+        isRefreshing = true;
+
+        try {
+          // instance 대신 axios 요청
+          // refreshToken으로 accessToken 재발급 요청
+          const { data } = await axios.get(`${process.env.NEXT_PUBLIC_SERVER_DOMAIN}/auth/token`, {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+          });
+
+          const newAccessToken = data.accessToken;
+          setCookie('admin-accessToken', newAccessToken, 'AT');
+
+          originalRequest.headers.authorization = `Bearer ${newAccessToken}`;
+          return axiosInstanceForAdmin(originalRequest);
+        } catch (error) {
+          // refreshToken 생성 실패 시,
+          removeCookie('admin-accessToken');
+          removeCookie('admin-refreshToken');
+        } finally {
+          isRefreshing = false;
+        }
+      }
+    }
     return Promise.reject(error);
   }
 );
